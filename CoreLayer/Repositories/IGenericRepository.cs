ccb0df7a98517d0183a238f00db4e15cf26338c3
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-10 23:15:38 +0300

    Created Custom classes for product and category

--- CoreLayer/Repositories/IGenericRepository.cs
+++ CoreLayer/Repositories/IGenericRepository.cs
@@ -4,7 +4,6 @@ namespace CoreLayer.Repositories
 {
     public interface IGenericRepository <T> where T : class
     {
-
         Task <T> GetByIdAsync (int id);
         IQueryable<T> GetAll();
         IQueryable<T> where(Expression<Func<T, bool>> expression);
---
+++ CoreLayer/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using CoreLayer.DTOs;
+using CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        public Task<CustomResponseDto<CategoryWithProductsDto>> GetSingleCategoryByIdWithProductsAsync(int categoryId);
+    }
+}
