d3493a526956bf330a4e13dec7dbe8bceade8a12
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-11 11:19:42 +0300

    Created services for custom services and adding fluent validations for product

---
+++ ServiceLayer/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CoreLayer.DTOs;
+using CoreLayer.Models;
+using CoreLayer.Repositories;
+using CoreLayer.Services;
+using CoreLayer.UnitOfWork;
+
+namespace ServiceLayer.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        {
+            var products = await _productRepository.GetProductsWitCategory();
+            var productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+
+        }
+    }
+}
